6151cc04a9a718c45f3cb8b1ceeb6739e4b77b07
Fábio <fabiosgcontato@gmail.com>
2023-07-01 23:03:50 -0300

    Project base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth';
+import { useAuthentication } from './hooks/useAuthentication';
+import './App.css'
 
-function App() {
+//context
+import { AuthProvider } from './context/AuthContext';
+
+
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import About from './pages/About/About'
+import Home from './pages/Home/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard';
+import CreatePost from './pages/CreatePost';
+import Search from './pages/Search';
+import Post from './pages/Post';
+import EditPost from './pages/EditPost';
+export default function App() {
+
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+
+  }, [auth])
+
+  if (loadingUser) {
+    return <p>Carregando...</p>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AuthProvider value={{ user }}>
+      <Router>
+        <div className="app container">
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/search' element={<Search />} />
+            <Route path='/posts/:id' element={<Post />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
+            <Route path='/register' element={!user ? <Register /> : <Navigate to="/" />} />
+            <Route path='/dashboard' element={user ? <Dashboard /> : <Navigate to="/login" />} />
+            <Route path='/post/create' element={user ? <CreatePost /> : <Navigate to="/login" />} />
+            <Route path='/posts/edit/:id' element={user ? <EditPost /> : <Navigate to="/login" />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </AuthProvider>
+  )
 }
-
-export default App;
---
+++ src/pages/Search/Search.jsx
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+import styles from './Search.module.css'
+
+//hooks
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import { useQuery } from '../../hooks/useQuery';
+
+//components
+import PostDetail from '../../components/PostDetail/PostDetail';
+
+export default function Search() {
+  const query = useQuery()
+  const search = query.get("q")
+  const { documents: posts } = useFetchDocuments("posts", search)
+  return (
+    <div className={styles.search_container}>
+      <h2>Search</h2>
+      <div>
+        {!posts && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts a partir da sua busca...</p>
+            <Link to='/' className='btn btn-dark'>Voltar</Link>
+          </div>
+        )}
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts a partir da sua busca...</p>
+            <Link to='/' className='btn btn-dark'>Voltar</Link>
+          </div>
+        )}
+        {posts &&
+          posts.map((post) => (
+            <PostDetail key={post.id} post={post} />
+          ))}
+      </div>
+    </div>
+  )
+}
